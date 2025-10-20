7bcd1c2faaa4ef4cdb6bac42effcc40f3387968c
HaBaPa <166997601+HaBaPa@users.noreply.github.com>
2025-10-20 22:42:01 +0700

    Update LoginResponse.java

--- src/main/java/com/atp/fwfe/dto/account/login/LoginResponse.java
+++ src/main/java/com/atp/fwfe/dto/account/login/LoginResponse.java
@@ -15,15 +15,6 @@ public class LoginResponse {
     private String message;
     private String role;
     private String username;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     private Long id;
-=======
->>>>>>> be9c2d22b390b8389679befff364e08bdff42788
-=======
->>>>>>> be9c2d22b390b8389679befff364e08bdff42788
-=======
     private Long id;
->>>>>>> dd9e548b27a8559239cd2901f94860b9b455b161
 }
